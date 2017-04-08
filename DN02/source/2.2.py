2a4dff3d60aeaea39c3a82e2fe0ba295b51c38b9
ppecar <pp1508@student.uni-lj.si>
2017-04-08 12:15:41 +0200

    Druga narejena

--- DN02/source/2.2.py
+++ DN02/source/2.2.py
@@ -1,7 +1,11 @@
 from datetime import datetime
+from locale import currency
+
+from Tools.pynche.ColorDB import get_colordb
 from numpy import *
 from collections import defaultdict
 
+from openpyxl.utils.cell import get_column_letter
 
 set_printoptions(suppress=True)
 
@@ -12,13 +16,54 @@ movieID=[int(i) for i in data[:,1]]
 ratingID=[float(i) for i in data[:,2]]
 timestamp=[datetime.fromtimestamp(i).strftime('%Y-%m-%d')
            for i in data[:,3]]
-relevantData=zip(movieID,ratingID)
+relevantData=zip(userID,movieID,ratingID)
+temp=set(userID)
+#print(len(list(temp)))
 #vse skupne ocene, št. pojavitev
-allMovies=defaultdict(int)
-for mID,rating in relevantData:
-    if(mID not in allMovies):
-        allMovies[mID]=0
-    allMovies[mID]=+1
+allMovies=defaultdict(list)
+for uID,mID,rating in relevantData:
+    if len(allMovies[mID])!=2:
+        allMovies[mID].append([])
+        allMovies[mID].append([])
+        allMovies[mID][0]=["?" for i in range(0,len(list(temp))+1)]
+    allMovies[mID][1]=[0]
 
-topTrueFilmi = sorted(allMovies.items(), key=lambda v: v[1], reverse=False)[:100]
-print(topTrueFilmi)
+ikd=zip(userID,movieID,ratingID)
+for id,mv,rt in ikd:
+    tem1p= allMovies[mv]
+    allMovies[mv][0][id]=rt
+    allMovies[mv][1][0]+=1
+
+
+
+topTrueFilmi = sorted(allMovies.items(), key=lambda v: v[1][1][0], reverse=True)[:100]
+
+
+topVrstica=[]
+topVrstica.append("movieID")
+for i in range(0,len(topTrueFilmi[0][1][0])+1):
+    topVrstica.append("u" + str(i))
+from openpyxl import *
+wb = Workbook()
+dest_filename = 'testing_data.xlsx'
+ws1 = wb.active
+ws1.title = "range names"
+for ind,val in enumerate(topVrstica):
+    temp = get_column_letter(ind+1)
+    ws1[temp+str(1)].value=val
+
+currentIndex=2
+forKeys=1
+for key,ratings in topTrueFilmi:
+    temp = get_column_letter(forKeys)
+    ws1[temp + str(currentIndex)].value = key
+    for ind,rating in enumerate(ratings[0]):
+        ind=ind+1
+        temp = get_column_letter(ind + 1)
+        ws1[temp + str(currentIndex)].value = rating
+    currentIndex+=1
+
+wb.save(filename=dest_filename)
+
+#for i in topTrueFilmi:
+#    print(i[1][1][0])
